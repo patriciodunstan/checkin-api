bda54611807eae653983f8e4898f1e9be2671dba
patriciodunstan <patricio.dunstan@gmail.com>
2025-08-23 17:50:46 -0400

    feat: Update FlightControllerTest annotation for improved clarity

--- src/test/java/com/andesairlines/checkin_api/flight/controller/FlightControllerTest.java
+++ src/test/java/com/andesairlines/checkin_api/flight/controller/FlightControllerTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(FlightController.class)
+@WebMvcTest(controllers = FlightController.class)
 class FlightControllerTest {
 
     @Autowired
