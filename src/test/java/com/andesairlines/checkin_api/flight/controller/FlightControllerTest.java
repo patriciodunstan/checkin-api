980341b051b25f3472c0447da6277c30d14c9f28
patriciodunstan <patricio.dunstan@gmail.com>
2025-08-23 21:14:06 -0400

    fix test an application

--- src/test/java/com/andesairlines/checkin_api/flight/controller/FlightControllerTest.java
+++ src/test/java/com/andesairlines/checkin_api/flight/controller/FlightControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(FlightController.class)
+@ActiveProfiles("test")
 class FlightControllerTest {
 
     @Autowired
@@ -28,7 +30,6 @@ class FlightControllerTest {
 
     @MockBean
     private ManualSeatAssignmentService manualSeatAssignmentService;
-
 
     private FlightResponse testFlightResponse;
     private PassengerResponse testPassengerResponse;
